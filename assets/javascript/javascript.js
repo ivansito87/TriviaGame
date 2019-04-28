d803357ce9ce33771c0d19eca133451fae0411f6
Ivan Rendon <ivanrendon@Ivans-iMac.attlocal.net>
2019-04-28 18:46:58 -0400

    fully functional additional styling prior to final product

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,234 +1,248 @@
-var allQuestions = [{
-    question: "Before Mt. Everest was discovered, whaich mountain was considered to be the highest mountain in the world?",
-    choices: ["Mt. Kilimanjaro", "Kanchenjunga", "Mount Everest"],
-    correctAnswer: 1
-},
 
-{
-    question: "Does England have a 4th of July?",
-    choices: ["Yes", "No", "I don't know"],
-    correctAnswer: 0
-},
+// Core JavaScript
 
-{
-    question: "What is Rupert the bear's middle name?",
-    choices: ["Bear", "He doesn't have one!", "The", "Rupert"],
-    correctAnswer: 2
-},
-
-{
-    question: " What can you never eat for breakfast? ",
-    choices: ["Dinner", "Something sugary", "Lunch", "Supper"],
-    correctAnswer: 0
-},
-
-{
-    question: "If there are three apples and you took two away, how many do you have?",
-    choices: ["One", "Two", "None"],
-    correctAnswer: 1
-},
-
-{
-    question: "Spell 'Silk' out loud, 3 times in a row. What do cows drink?",
-    choices: ["Milk", "Water", "Juice", "Cows can't drink"],
-    correctAnswer: 1
-},
-
-{
-    question: "Which is heavier, 100 pounds of rocks or 100 pounds of gold? ",
-    choices: ["100 pounds of rocks", "100 pounds of rocks", "They weigh the same"],
-    correctAnswer: 2
-},
-
-{
-    question: "Can you spell 80 in two letters?",
-    choices: ["AI-TY", "It's not possible", "EIGH-TY", "A-T"],
-    correctAnswer: 3
-},
-
-{
-    question: "What question must always be answered ''Yes''?",
-    choices: ["What does Y-E-S spell?", "Will everyone die someday?", "Does everyone have a biological mother?", "Are you a human?"],
-    correctAnswer: 0
-},
-
-{
-    question: "How many sides does a circle have?",
-    choices: ["The back", "None. It's a circle", "Two", "Four"],
-    correctAnswer: 2
-},
-
-{
-    question: "What has a tail but no body?",
-    choices: ["A human", "A coin", "A cloud"],
-    correctAnswer: 1
-},
-
-{
-    question: "What word in the English language is always spelled incorrectly?",
-    choices: ["It's possible to spell anything right as long as you learn it", "Shakespeare", "Onomatopoeia", "Incorrectly"],
-    correctAnswer: 3
-},
-
-{
-    question: "When do you stop at green and go at red?",
-    choices: ["Watermelon!", "Traffic light!", "Garden"],
-    correctAnswer: 0
-},
-
-{
-    question: "What rotates but still remains in the same place?",
-    choices: ["Bottle (spin the bottle game)", "Clock", "Stairs"],
-    correctAnswer: 2
-},
-
-{
-    question: "How can you lift an elephant with one hand?",
-    choices: ["Truck", "Use both hands!", "Use a lever", "There is no such thing"],
-    correctAnswer: 3
-}
-];
-
-var currentQuestion = 0;
-
-var countDown = 15;
-console.log(countDown);
-
-var intervalId;
-
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-async function demo() {
-    console.log('Taking a break...');
-   
-    await sleep(3000);
-    console.log('Two seconds later');
-    $("#time").text("");
-    nextQuestion();
-}
+let userName = prompt(`Welcome!
+    Please Enter your name!`);
+var uppercaseUserName = userName.charAt(0).toUpperCase() + userName.slice(1);
+$("#user").text(uppercaseUserName);
 
 
 $(document).ready(function () {
-    $(".btns").on("click", function () {
 
+    console.log("ready!");
 
+    $("#btnR").hide();
 
+    var allQuestions = [{
+        question: "Before Mt. Everest was discovered, whaich mountain was considered to be the highest mountain in the world?",
+        choices: ["Mt. Kilimanjaro", "Kanchenjunga", "Mount Everest"],
+        correctAnswer: 1
+    },
 
+    {
+        question: "Does England have a 4th of July?",
+        choices: ["Yes", "No", "I don't know"],
+        correctAnswer: 0
+    },
 
+    {
+        question: "What is Rupert the bear's middle name?",
+        choices: ["Bear", "He doesn't have one!", "The", "Rupert"],
+        correctAnswer: 2
+    },
 
-        console.log("Working!")
-        gameStarts();
+    {
+        question: " What can you never eat for breakfast? ",
+        choices: ["Dinner", "Something sugary", "Lunch", "Supper"],
+        correctAnswer: 0
+    },
 
+    {
+        question: "If there are three apples and you took two away, how many do you have?",
+        choices: ["One", "Two", "None"],
+        correctAnswer: 1
+    },
 
+    {
+        question: "Spell 'Silk' out loud, 3 times in a row. What do cows drink?",
+        choices: ["Milk", "Water", "Juice", "Cows can't drink"],
+        correctAnswer: 1
+    },
 
+    {
+        question: "Which is heavier, 100 pounds of rocks or 100 pounds of gold? ",
+        choices: ["100 pounds of rocks", "100 pounds of rocks", "They weigh the same"],
+        correctAnswer: 2
+    },
+
+    {
+        question: "Can you spell 80 in two letters?",
+        choices: ["AI-TY", "It's not possible", "EIGH-TY", "A-T"],
+        correctAnswer: 3
+    },
+
+    {
+        question: "What question must always be answered ''Yes''?",
+        choices: ["What does Y-E-S spell?", "Will everyone die someday?", "Does everyone have a biological mother?", "Are you a human?"],
+        correctAnswer: 0
+    },
+
+    {
+        question: "How many sides does a circle have?",
+        choices: ["The back", "None. It's a circle", "Two", "Four"],
+        correctAnswer: 2
+    },
+
+    {
+        question: "What has a tail but no body?",
+        choices: ["A human", "A coin", "A cloud"],
+        correctAnswer: 1
+    },
+
+    {
+        question: "What word in the English language is always spelled incorrectly?",
+        choices: ["It's possible to spell anything right as long as you learn it", "Shakespeare", "Onomatopoeia", "Incorrectly"],
+        correctAnswer: 3
+    },
+
+    {
+        question: "When do you stop at green and go at red?",
+        choices: ["Watermelon!", "Traffic light!", "Garden"],
+        correctAnswer: 0
+    },
+
+    {
+        question: "What rotates but still remains in the same place?",
+        choices: ["Bottle (spin the bottle game)", "Clock", "Stairs"],
+        correctAnswer: 2
+    },
+
+    {
+        question: "How can you lift an elephant with one hand?",
+        choices: ["Truck", "Use both hands!", "Use a lever", "There is no such thing"],
+        correctAnswer: 3
+    }
+    ];
+
+    var currentQuestion = 0;
+
+    var countDown = 15;
+
+    var intervalId;
+
+    var rightQuestions = 0;
+
+    var wrongQuestions = 0;
+
+    var right = new Audio("assets/sounds/ding.mp3");
+
+    var wrong = new Audio("assets/sounds/wrong.mp3");
+
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    async function demo() {
+        console.log('Taking a break...');
+
+        await sleep(3000);
+        console.log('Two seconds later');
+        $("#time").text("");
+        nextQuestion();
+    }
+
+    $(document).ready(function () {
+        $(".btns").on("click", function () {
+
+            console.log("Working!")
+            gameStarts();
+
+        });
     });
-});
 
-function decrement() {
-    $("#time").text(countDown);
-    countDown--;
+    function decrement() {
+        $("#time").text(countDown);
+        countDown--;
 
-    if (countDown < 0) {
-        userChoice(-1);
+        if (countDown < 0) {
+            userChoice(-1);
+            countDown = 15;
+        }
+    }
+
+    function setTime() {
         countDown = 15;
-    }    
-}
+        intervalId = setInterval(decrement, 1000);
+    }
 
-function setTime() {
-    countDown = 15;
-    intervalId = setInterval(decrement, 1000);
-}
+    function gameStarts() {
+        countDown = 15;
+        rightQuestions = 0;
+        wrongQuestions = 0;
+        currentQuestion = 0;
+        setTime();
+        $("#question").text(allQuestions[currentQuestion].question);
+        $(".btns").hide();
+        $("#btnR").hide()
+        renderChoices();
+    }
 
-// setInterval(function(){
-//     countDown--;
-// },1000);
+    function renderChoices() {
+        let count = 0;
+        allQuestions[currentQuestion].choices.forEach(function (element) {
+            console.log(element);
+            $("#choice" + count).text(`\u21D2 ${element}`);
+            count++;
+        });
+    }
 
-// function setTime() {
-//     setTimeout(function () {
-//         userChoice(-1);
-        
-//     }, 3000);
-// }
+    function userChoice(userPicked) {
+        clearChoices();
+        clearInterval(intervalId);
+        console.log("testing user" + userPicked);
+        console.log("testing machine" + allQuestions[currentQuestion].correctAnswer);
+        allQuestions[currentQuestion].correctAnswer
+        if (parseInt(userPicked) === allQuestions[currentQuestion].correctAnswer) {
+            console.log("works!")
+            rightQuestions++;
+            right.play();
+            $("#choice1").html('<img src ="assets/images/correct.png" width = "400px">');
+            $("#question").html(`<h2>Yup! you got it right. </h2>`);
+        } else {
+            wrongQuestions++;
+            wrong.play()
+            $("#choice1").html('<img src ="assets/images/wrong.png" width = "400px">');
+            $("#question").text(`Nope! the correct answer is: ` + allQuestions[currentQuestion].choices[allQuestions[currentQuestion].correctAnswer]);
+        }
+        demo();
+    }
 
+    $(".choices").on("click", function () {
+        let userPicked = $(this).attr("data-answer");
+        console.log(userPicked);
+        userChoice(userPicked);
 
-function gameStarts() {
-    setTime();
-    $("#question").text(allQuestions[0].question);
-    $(".btns").hide();
-    renderChoices();
-}
-
-function renderChoices() {
-    let count = 0;
-    allQuestions[currentQuestion].choices.forEach(function (element) {
-        console.log(element);
-        $("#choice" + count).text(`\u21D2 ${element}`);
-        count++;
     });
 
-}
-
-function userChoice(userPicked) {
-    clearChoices();
-    clearInterval(intervalId);
-    console.log("testing user" + userPicked);
-    console.log("testing machine" + allQuestions[currentQuestion].correctAnswer);
-    allQuestions[currentQuestion].correctAnswer
-    if (parseInt(userPicked) === allQuestions[currentQuestion].correctAnswer) {
-        console.log("works!")
-        $("#question").html(`<h2>Yup! you got it right. </h2>`);
-    } else {
-        $("#question").text(`Nope! the correct answer is: ` + allQuestions[currentQuestion].choices[allQuestions[currentQuestion].correctAnswer]);
+    function clearChoices() {
+        $(".choices").text("");
     }
-    
-    demo();
 
-}
+    function gameEnds() {
+        console.log("Game over!");
+        clearChoices();
+        let finalScore = rightQuestions + wrongQuestions;
+        $("#question").html(`<h1>Game Over! <strong>You Got ${rightQuestions} out of ${finalScore}</strong></h1>`);
+        $("#btnR").show();
+    }
 
-$(".choices").on("click", function () {
-    let userPicked = $(this).attr("data-answer");
-    console.log(userPicked);
-    userChoice(userPicked);
+    $("#btnR").on("click", function () {
+        gameStarts();
+    });
 
-});
-
-function clearChoices() {
-    $(".choices").text("");
-    
-
-}
-
-function gameEnds() {
-    console.log("Game over!");
-}
-
-function nextQuestion() {
-    if (currentQuestion < allQuestions.length) {
+    function nextQuestion() {
         currentQuestion++;
-    } else {
-        gameEnds();
-
+        if (currentQuestion < allQuestions.length) {
+            console.log(allQuestions.length)
+            setTime();
+            $("#question").text(allQuestions[currentQuestion].question);
+            renderChoices();
+        } else {
+            gameEnds();
+        }
     }
-    setTime();
-    $("#question").text(allQuestions[currentQuestion].question);
-    renderChoices();
-}
 
+    $(document).scroll(function () {
+        navbarScroll();
+    });
 
+    function navbarScroll() {
+        var y = window.scrollY;
+        if (y > 10) {
+            $('.header').addClass('small');
+        } else if (y < 10) {
+            $('.header').removeClass('small');
+        }
+    }
 
-
-
-$(document).scroll(function () {
-    navbarScroll();
 });
-
-function navbarScroll() {
-    var y = window.scrollY;
-    if (y > 10) {
-        $('.header').addClass('small');
-    } else if (y < 10) {
-        $('.header').removeClass('small');
-    }
-}
