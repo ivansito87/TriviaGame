9245c701d01272768d016b8e78adc027aeeb8f7d
Ivan Rendon <ivanrendon@Ivans-iMac.attlocal.net>
2019-04-29 16:55:57 -0400

    adds social media links and final style

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -247,7 +247,38 @@ $(document).ready(function () {
 
 });
 
+$(".choices").mouseover(function () { $(this).animate({ marginLeft: '20px' }, 'fast') });
+$(".choices").mouseout(function () { $(this).animate({ marginLeft: '0px'}, 0) });
 
 
 
-////==========================================> Mouse Trail
+////==========================================> Mouse Trail  º(^.^)º
+//  Mouse Trail effect º(^.^)º   -------> 
+$(document).ready(function () {
+    var mousePos = {};
+    function getRandomInt(min, max) {
+        return Math.round(Math.random() * (max - min + 1)) + min;
+    }
+    $(window).mousemove(function (e) {
+        mousePos.x = e.pageX;
+        mousePos.y = e.pageY;
+    });
+    $(window).mouseleave(function (e) {
+        mousePos.x = -1;
+        mousePos.y = -1;
+    });
+    var draw = setInterval(function () {
+        if (mousePos.x > 0 && mousePos.y > 0) {
+            var range = 15;
+            var color = "background: rgb(" + getRandomInt(204, 205) + "," + getRandomInt(253, 254) + "," + getRandomInt(255, 255) + ");";
+            var sizeInt = getRandomInt(1, 7);
+            size = "height: " + sizeInt + "px; width: " + sizeInt + "px;";
+            var left = "left: " + getRandomInt(mousePos.x - range - sizeInt, mousePos.x + range) + "px;";
+            var top = "top: " + getRandomInt(mousePos.y - range - sizeInt, mousePos.y + range) + "px;";
+            var style = left + top + color + size;
+            $("<div class='ball' style='" + style + "'></div>").appendTo('#wrap').one("webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend", function () { $(this).remove(); });
+        }
+    }, 1);
+});
+
+// ---------- >>>> Thank you! <<<<<------------------\\
